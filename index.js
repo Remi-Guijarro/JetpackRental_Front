b56bd8a52ab14b0cd3d0f2f8dac991909d81629c
Remi-Guijarro <remi.guijarro-espinosa@etu.univ-amu.fr>
2019-10-12 18:27:22 +0200

    Implemented fonctionnal test for the update feature ; 	- test if modal shows 	- test if modal contais input field with correct values according to jetpack object

--- index.js
+++ index.js
@@ -4,21 +4,22 @@ const HttpClient = require('./src/HttpClient');
 
 const httpClient = new HttpClient(appConfig.apiUrl);
 const jetpackService = new JetpackService(httpClient);
+const launchModal = function(){
+    $('#modalImgUrl').val($(this).data('jetPackImg'));
+    $('#modalJetName').val($(this).data('jetPackName'));
+    $('#editJetModal').modal('toggle');
+};
 jetpackService.getJetpacks().then(jetpacks => {
     $.each(jetpacks,(index,jetpack) => {
-        let jetPackDiv = $('<div class="card" style="width: 18rem; margin: auto;"></div>');
+        const jetPackDiv = $('<div class="card" style="width: 18rem; margin: auto;"></div>');
         jetPackDiv.append(' <img src="'+ jetpack.image +'" style="width: 18rem;" class="card-img-top" alt="...">');
-        let jetPackDivBody =$(' <div class="card-body"> <h5 class="card-title">' + jetpack.name + '</h5> </div>');
-        jetPackDivBodyEditButton = $('<button class="btn btn-primary edit-jet-button">Edit</button>');
+        const jetPackDivBody =$(' <div class="card-body"> <h5 class="card-title">' + jetpack.name + '</h5> </div>');
+        const jetPackDivBodyEditButton = $('<button class="btn btn-primary edit-jet-button">Edit</button>');
         jetPackDivBodyEditButton.data('jetPackName',jetpack.name);
         jetPackDivBodyEditButton.data('jetPackImg',jetpack.image);
-        jetPackDivBodyEditButton.click(function() {
-            $('#modal-img-url').val($(this).data('jetPackImg'));
-            $('#modal-jet-name').val($(this).data('jetPackName'));
-            $('#editJetModal').modal('toggle');
-        });
+        jetPackDivBodyEditButton.click(launchModal);
         jetPackDivBody.append(jetPackDivBodyEditButton);
         jetPackDiv.append(jetPackDivBody);
-        $('#jetpacks').append(jetPackDiv)
+        $('#jetpacks').append(jetPackDiv);
     });
 });
--- tests/ui/integration/ui/jetpack-update.spec.js
+++ tests/ui/integration/ui/jetpack-update.spec.js
@@ -3,7 +3,32 @@ context('Jetpack update', () => {
         cy.visit('/');
     });
 
-    it('update button should be available', () => {
-        cy.get('.edit-jet-button').should('be.visible');
+    const editButtonClass = '.edit-jet-button';
+    const visibleState = 'be.visible';
+
+    it('update buttons should be available', () => {
+        cy.get(editButtonClass).should(visibleState);
+    });
+
+    it('modal should open when edit button is clicked', () => {
+        cy.get(editButtonClass).first().click();
+        cy.get('#editJetModal').should(visibleState);
+    });
+
+    it('modal should open when edit button is clicked', () => {
+        cy.get(editButtonClass).first().click();
+        const modal = cy.get('#editJetModal');
+        modal.should(visibleState);
+    });
+
+    const imgUrl = 'https://gamepedia.cursecdn.com/fortnite_gamepedia/e/e1/Jetpack_icon.png';
+    it('modal should contains jepack informations ', function () {
+        cy.get(editButtonClass).first().click();
+        cy.get('#modalImgUrl').invoke('val').then(value => {
+            expect(value).to.be.equal(imgUrl);
+        });
+        cy.get('#modalJetName').invoke('val').then(value => {
+            expect(value).to.be.equal('Jetpack Fortnite Wiki');
+        });
     });
 });
