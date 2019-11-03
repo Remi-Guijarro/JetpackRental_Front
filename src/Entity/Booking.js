29aec9107c1a8b61b588f97120e7263ea26066ec
Remi-Guijarro <remi.guijarro-espinosa@etu.univ-amu.fr>
2019-11-03 20:22:43 +0100

    Implemented Booking entity

---
+++ src/Entity/Booking.js
@@ -0,0 +1,40 @@
+module.exports = class {
+    constructor() {
+        this._id = null;
+        this._jetpackId = null;
+        this._start_date_time = null;
+        this._end_date_time = null;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    get jetpackId() {
+        return this._jetpackId;
+    }
+
+    get start_date_time() {
+        return this._start_date_time;
+    }
+
+    get end_date_time() {
+        return this._end_date_time;
+    }
+
+    set id(value) {
+        this._id = value;
+    }
+
+    set jetpackId(value) {
+        this._jetpackId = value;
+    }
+
+    set start_date_time(value) {
+        this._start_date_time = value;
+    }
+
+    set end_date_time(value) {
+        this._end_date_time = value;
+    }
+};
