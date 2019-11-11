099d6a759dac9b72ee8be7fec25510a4dc0ba15e
Remi-Guijarro <remi.guijarro-espinosa@etu.univ-amu.fr>
2019-11-11 18:26:24 +0100

    Fixed errors with unit tests

--- src/Service/Api/jetpackApi.test.js
+++ src/Service/Api/jetpackApi.test.js
@@ -1,149 +1,119 @@
 const JetpackApi = require('./JetpackApi');
-const JetpackEntity = require('../../Entity/Jetpack');
-
-const testJetpackId_1 = '123';
-const testJetpackName_1 = 'Test Jetpack 01';
-const testImageUrl_1 = 'test/url/image01.jpg';
-
-const testJetpackId_2 = '456';
-const testJetpackName_2 = 'Test Jetpack 02';
-const testImageUrl_2 = 'test/url/image02.jpg';
-
-const testUpdatedJetpackName = 'Test Updated Jetpack';
-const testNewImageUrl = 'test/url/newImage.jpg';
-
+const Jetpack = require('../../Entity/Jetpack');
 describe('getJetpacks function', () => {
     test('should return an array of size 1', () => {
         const httpClientMock = {
             fetch: jest.fn()
         };
-
         httpClientMock.fetch.mockResolvedValue([
             {
-                id: testJetpackId_1,
-                name: testJetpackName_1,
-                image: testImageUrl_1
+                id: '123',
+                name: 'The Jetpack',
+                image: 'base64 ...'
             }
         ]);
-
         const jetpackApi = new JetpackApi(httpClientMock);
-
         return jetpackApi.getJetpacks()
             .then(resp => {
                 expect(Array.isArray(resp)).toBe(true);
                 expect(resp.length).toBe(1);
-        });
+            });
     });
 
     test('should return an array of size 2', () => {
         const httpClientMock = {
             fetch: jest.fn()
         };
-
         httpClientMock.fetch.mockResolvedValue([
             {
-                id: testJetpackId_1,
-                name: testJetpackName_1,
-                image: testImageUrl_1
+                id: '123',
+                name: 'The First Jetpack',
+                image: 'base64 ...'
             },
             {
-                id: testJetpackId_2,
-                name: testJetpackName_2,
-                image: testImageUrl_2
+                id: '456',
+                name: 'The Second Jetpack',
+                image: 'base64 ...'
             }
         ]);
-
         const jetpackApi = new JetpackApi(httpClientMock);
-
         return jetpackApi.getJetpacks()
             .then(resp => {
                 expect(Array.isArray(resp)).toBe(true);
                 expect(resp.length).toBe(2);
-        });
+            });
     });
-
     test('should return the jetpacks with correct values', () => {
         const httpClientMock = {
             fetch: jest.fn()
         };
-
         httpClientMock.fetch.mockResolvedValue([
             {
-                id: testJetpackId_1,
-                name: testJetpackName_1,
-                image: testImageUrl_1
+                id: '123',
+                name: 'The First Jetpack',
+                image: 'base64 ...'
             },
             {
-                id: testJetpackId_2,
-                name: testJetpackName_2,
-                image: testImageUrl_2
+                id: '456',
+                name: 'The Second Jetpack',
+                image: 'base64 ...'
             }
         ]);
-
         const jetpackApi = new JetpackApi(httpClientMock);
-
         return jetpackApi.getJetpacks()
             .then(resp => {
-                expect(resp[0]).toBeInstanceOf(JetpackEntity);
-                expect(resp[0].id).toBe(testJetpackId_1);
-                expect(resp[0].name).toBe(testJetpackName_1);
-                expect(resp[0].image).toBe(testImageUrl_1);
-
-                expect(resp[1]).toBeInstanceOf(JetpackEntity);
-                expect(resp[1].id).toBe(testJetpackId_2);
-                expect(resp[1].name).toBe(testJetpackName_2);
-                expect(resp[1].image).toBe(testImageUrl_2);
+                expect(resp[0]).toBeInstanceOf(Jetpack);
+                expect(resp[0].id).toBe('123');
+                expect(resp[0].name).toBe('The First Jetpack');
+                expect(resp[0].image).toBe('base64 ...');
+                expect(resp[1]).toBeInstanceOf(Jetpack);
+                expect(resp[1].id).toBe('456');
+                expect(resp[1].name).toBe('The Second Jetpack');
+                expect(resp[1].image).toBe('base64 ...');
             });
-       });
+    });
 });
-
 describe('updateJetpack function', () => {
     test('return should not be null or undefined', () => {
         const httpClientMock = {
             fetch: jest.fn().mockResolvedValue(
                 {
-                    id: testJetpackId_1,
-                    name: testUpdatedJetpackName,
-                    image: testNewImageUrl
+                    id: '123',
+                    name: 'Updated Jetpack',
+                    image: 'base64 ...'
                 }
             )
         };
         const jetpackApi = new JetpackApi(httpClientMock);
-        const jetpack = new JetpackEntity();
-
-        jetpack.id = testJetpackId_1;
-        jetpack.name = testUpdatedJetpackName;
-        jetpack.image = testNewImageUrl;
-
+        const jetpack = new Jetpack();
+        jetpack.id = 123;
+        jetpack.image = 'base64 ..';
+        jetpack.name = 'Updated Jetpack';
         return jetpackApi.updateJetPack(jetpack).then(resp => {
             expect(resp).not.toBeNull();
             expect(resp).not.toBeUndefined();
         });
     });
-
     test('Test UpdateJetpack successful update', () => {
         const httpClientMock = {
             fetch: jest.fn().mockResolvedValue(
                 {
-                    id: testJetpackId_1,
-                    name: testUpdatedJetpackName,
-                    image: testNewImageUrl
+                    id: '123',
+                    name: 'Updated Jetpack',
+                    image: 'newImg.jpg'
                 }
             )
         };
-
         const jetpackApi = new JetpackApi(httpClientMock);
-        const jetpack = new JetpackEntity();
-
-        jetpack.id = testJetpackId_1;
-        jetpack.name = testUpdatedJetpackName;
-        jetpack.image = testNewImageUrl;
-
+        const jetpack = new Jetpack();
+        jetpack.id = 123;
+        jetpack.name = 'Updated Jetpack';
+        jetpack.image = 'newImg.jpg';
         return jetpackApi.updateJetPack(jetpack).then(resp => {
-            // The jetpack is sent to the back-end and returned as is
-            expect(resp.id).toBe(testJetpackId_1);
-            expect(resp.name).toBe(testUpdatedJetpackName);
-            expect(resp.image).toBe(testNewImageUrl);
+            // The jetpack is sent to the back and should be unchanged
+            expect(resp.id).toBe('123');
+            expect(resp.name).toBe('Updated Jetpack');
+            expect(resp.image).toBe('newImg.jpg');
         });
     });
 });
@@ -195,32 +165,6 @@ describe('saveJetpacks function', () => {
         return jetpackApi.saveJetpack(jetpack)
             .then(returnedJetpack => {
                 expect(returnedJetpack.id).not.toBeNull();
-            });
-    });
-});
-
-describe('getBookingByDateTimeRange', () => {
-    test('should return an array', () => {
-        const httpClientMock = {
-            fetch: jest.fn()
-        };
-
-        httpClientMock.fetch.mockResolvedValue([
-            {
-                id: '123',
-                name: 'The Jetpack',
-                image: 'base64 ...'
-            }
-        ]);
-
-        const jetpack = new Jetpack();
-        jetpack.name = 'Updated Jetpack';
-        jetpack.image = 'newImg.jpg';
-        const jetpackApi = new JetpackApi(httpClientMock);
-
-        return jetpackApi.saveJetpack(jetpack)
-            .then(returnedJetpack => {
-                expect(Array.isArray(returnedJetpack)).toBe(true);
             });
     });
 });
